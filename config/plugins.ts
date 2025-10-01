ccf505949dd56ba6e96f4eb632e4c9aa7b2c2897
SAWADOGO Ahmad Abdoul Latif <sawahmad51@mail.com>
2025-10-01 19:14:09 +0000

    mise en plase du système de renitialisation de mot de passe

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,19 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME', 'sabdoullatif51@gmail.com'),
+          pass: env('SMTP_PASSWORD', 'dzjw ckrk tcqp ojlx'),
+        },
+      },
+      settings: {
+        defaultFrom: 'sabdoullatif51@gmail.com',
+        defaultReplyTo: 'sabdoullatif51@gmail.com',
+      },
+    },
+  },
+});
