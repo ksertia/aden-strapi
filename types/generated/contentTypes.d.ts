d40c71ee3af83f8e35cfcb30fbaba35c724fe594
Bikiega Faril <adamafarilb@gmail.com>
2025-10-12 02:07:34 +0000

    enregistrement du comtentTypes

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1589,7 +1589,7 @@ export interface PluginUsersPermissionsUser
       'plugin::users-permissions.user'
     > &
       Schema.Attribute.Private;
-    nodeId: Schema.Attribute.String;
+    nodeId: Schema.Attribute.String & Schema.Attribute.Required;
     password: Schema.Attribute.Password &
       Schema.Attribute.Private &
       Schema.Attribute.SetMinMaxLength<{
