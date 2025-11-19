718f550c11db3822e51ede92285a655874f85630
Kabore Faical <faicalkabore2@gmail.com>
2025-11-19 10:55:14 +0000

    changement du logo au niveau du landing page

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -712,13 +712,13 @@ export interface ApiHeaderHeader extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     locale: Schema.Attribute.String;
     localizations: Schema.Attribute.Relation<'oneToMany', 'api::header.header'>;
-    Logo: Schema.Attribute.Media<'images' | 'files'> &
+    logoText: Schema.Attribute.String &
       Schema.Attribute.SetPluginOptions<{
         i18n: {
           localized: true;
         };
       }>;
-    logoText: Schema.Attribute.String &
+    logoTextPhoto: Schema.Attribute.Media<'images' | 'files'> &
       Schema.Attribute.SetPluginOptions<{
         i18n: {
           localized: true;
